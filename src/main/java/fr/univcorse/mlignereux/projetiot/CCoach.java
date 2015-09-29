04cfa02d67f6b078975059a8062af8f3bcd4b1a6
Max5210 <maxime.lignereux@gmail.com>
2015-09-29 14:09:09 +0200

    creer Entités

--- src/main/java/fr/univcorse/mlignereux/projetiot/CCoach.java
+++
@@ -1,7 +0,0 @@
-package fr.univcorse.mlignereux.projetiot;
-
-/**
- * Created by asus on 29/09/2015.
- */
-public class CCoach {
-}
---
+++ src/main/java/fr/univcorse/mlignereux/projetiot/Entity/CChrono.java
@@ -0,0 +1,7 @@
+package fr.univcorse.mlignereux.projetiot.Entity;
+
+/**
+ * Created by asus on 29/09/2015.
+ */
+public class CChrono {
+}
