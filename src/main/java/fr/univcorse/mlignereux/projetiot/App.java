799fdaf31f7ce9230ae4e6c307f06a1d9b8d8285
Max5210 <maxime.lignereux@gmail.com>
2015-09-29 13:57:52 +0200

    creer projet

---
+++ src/main/java/fr/univcorse/mlignereux/projetiot/App.java
@@ -0,0 +1,13 @@
+package fr.univcorse.mlignereux.projetiot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
